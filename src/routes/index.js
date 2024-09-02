5f8ceba744f01942126eb5afa93e1e7c22a8c58f
sba15 <sbaweb15@gmail.com>
2024-09-02 13:03:59 +0530

    change

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../common/navbar/index.js';
+import Home from '../pages/home/index.js';
+import Footer from '../../src/common/footer/index.js'
+
+const Main = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default Main;
