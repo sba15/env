2819a45ef616eecd5ca9e8662eb4fef32ab3efb8
sba15 <sbaweb15@gmail.com>
2024-09-11 17:54:48 +0530

    change

--- src/pages/Industries/index.js
+++ src/pages/Industries/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./style.scss";
 import Indus from "../../assets/videos/Industries.mp4";
 import Industries1 from "../../assets/images/Industries1.jpg";
-
-
+import Industries2 from "../../assets/images/Industries2.jpg";
 
 const Industries = () => {
   return (
@@ -13,7 +12,7 @@ const Industries = () => {
           <div class="init-ai-sectionIns1">
             <div className="Ins1">Industries</div>
             <div class="init-red-bg px-5">
-              Explore Industry-Specific Solutions and Insights
+              Explore Industry-Specific Solutions
             </div>
           </div>
           <video autoPlay muted loop>
@@ -24,16 +23,14 @@ const Industries = () => {
 
       <section>
         <div className="p-6 md:p-10 mb-5 bg-white rounded-lg shadow-md">
-          <div className="flex flex-col md:flex-row gap-6 md:gap-10">
-            <div className="md:w-1/2 w-full md:pl-6 mt-6 md:mt-6">
-              <ul className="text-2xl pl-5 list-disc space-y-5 text-left font-sans">
-                <p>
-                  We at SBA provide reliable IT services and security solutions
-                  for data centers, cloud, and information security practices
-                  that cater to:
-                </p>
-              </ul>
-              <ul className="Indusfirst">
+          <div className="flex flex-col md:flex-row gap-6 md:gap-10 items-center">
+            <div className="md:w-1/2 w-full md:pl-6 mt-6 md:mt-0 text-left">
+              <p className="text-xl md:text-2xl font-sans">
+                We at SBA provide reliable IT services and security solutions
+                for data centers, cloud, and information security practices that
+                cater to:
+              </p>
+              <ul className="text-xl md:text-3xl pl-5 list-disc space-y-2 mt-9">
                 <li>BFSI</li>
                 <li>Manufacturing</li>
                 <li>Media</li>
@@ -44,7 +41,77 @@ const Industries = () => {
             <div className="md:w-1/2 w-full p-1 md:p-4 flex justify-end">
               <div className="overflow-hidden rounded-lg">
                 <img
-                  src={Industries1}
+                  src={Industries1} // Replace with actual image path
+                  className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-105"
+                  loading="lazy"
+                  alt="Enterprise IT Solutions"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="p-6 md:p-10 mb-5 bg-white rounded-lg shadow-md">
+          <div className="flex flex-col md:flex-row gap-6 md:gap-10">
+            <div className="md:w-1/2 w-full p-1 md:p-4 flex justify-end">
+              <div className="overflow-hidden rounded-lg">
+                <img
+                  src={Industries2}
+                  className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-105"
+                  loading="lazy"
+                  alt="Enterprise ML and Generative AI"
+                />
+              </div>
+            </div>
+            <div className="md:w-1/2 w-full md:pl-6 mt-4 md:mt-6">
+              <h2 className="text-3xl font-bold mb-6 md:mb-9 text-left text-gray-600">
+                <b>BFSI</b>
+              </h2>
+              <ul className="text-2xl pl-5 list-disc space-y-5 text-left">
+                <p>
+                  SBA offers end-to-end services for the BFSI industry,
+                  including digital transformation, IT consulting,
+                  cybersecurity, data analytics, and regulatory compliance
+                </p>
+                <p>
+                  These solutions enhance operational efficiency, ensure data
+                  security, and support seamless integration of advanced
+                  technologies, helping financial institutions stay competitive
+                  and compliant
+                </p>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="p-6 md:p-10 mb-5 bg-white rounded-lg shadow-md">
+          <div className="flex flex-col md:flex-row gap-6 md:gap-10">
+            <div className="md:w-1/2 w-full md:pl-6 mt-4 md:mt-6">
+              <h2 className="text-3xl font-bold mb-6 md:mb-9 text-left text-gray-600">
+                <b>BFSI</b>
+              </h2>
+              <ul className="text-2xl pl-5 list-disc space-y-5 text-left">
+                <p>
+                  SBA offers end-to-end services for the BFSI industry,
+                  including digital transformation, IT consulting,
+                  cybersecurity, data analytics, and regulatory compliance
+                </p>
+                <p>
+                  These solutions enhance operational efficiency, ensure data
+                  security, and support seamless integration of advanced
+                  technologies, helping financial institutions stay competitive
+                  and compliant
+                </p>
+              </ul>
+            </div>
+            <div className="md:w-1/2 w-full p-1 md:p-4 flex justify-end">
+              <div className="overflow-hidden rounded-lg">
+                <img
+                  src={Industries2}
                   className="w-full h-full object-cover transform transition-transform duration-300 hover:scale-105"
                   loading="lazy"
                   alt="Enterprise ML and Generative AI"
